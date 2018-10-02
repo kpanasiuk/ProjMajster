bc5dcbdac7e1e2647555da71dd9ba39e1a0410fd
Микола Панасюк <kolia.panasiuk@me.com>
2018-10-02 16:59:16 +0300

    support GHC flags

--- src/Main.hs
+++ src/Main.hs
@@ -13,33 +13,36 @@ out_dir     = build_dir </> "bas"
 prj_prj_dir = "prj" </> "shake"
 prj_file    = prj_prj_dir </> "Main.hs"
 stack_flags = prj_prj_dir </> "stack.flags"
+ghc_flags   = prj_prj_dir </> "ghc.flags"
 bas_exe     = out_dir </> "bas"
 
 main :: IO ()
 main = do
     cd <- getCurrentDirectory
-    maybePrjDir <- findM doesPrjFileExist . parents $ cd
-    case maybePrjDir of
-        Just prjDir -> setCurrentDirectory prjDir >> compile >> run
-        Nothing     -> report_error "A project location was not found."
+    mb_prj_dir <- findM does_prj_file_exist . parents $ cd
+    case mb_prj_dir of
+        Just prj_dir -> setCurrentDirectory prj_dir >> compile >> run
+        Nothing      -> report_error "A project location was not found."
                     "You need to be in a project directory to run this command."
   where
-    parents              = reverse . scanl1 combine . splitDirectories
-    doesPrjFileExist dir = doesFileExist $ dir </> prj_file
+    parents                 = reverse . scanl1 combine . splitDirectories
+    does_prj_file_exist dir = doesFileExist $ dir </> prj_file
 
 compile :: IO ()
 compile = do
     createDirectoryIfMissing True out_dir
-    stackFlags <- words <$> readFile stack_flags <|> pure []
+    s_flags <- words <$> readFile stack_flags <|> pure []
+    c_flags <- words <$> readFile ghc_flags <|> pure []
     command "stack" $
-        ["ghc"] ++ stackFlags ++ ["--"] ++
-        ["-hide-all-packages" | not . null $ stackFlags] ++
+        ["ghc"] ++ s_flags ++ ["--"] ++
+        ["-hide-all-packages" | not . null $ s_flags] ++
         [ "--make", prj_file
         , ("-i" ++) . takeDirectory $ prj_file
         , "-rtsopts", "-with-rtsopts=-I0"
         , "-outputdir", out_dir
         , "-o", bas_exe
-        ]
+        ] ++
+        c_flags
 
 run :: IO ()
 run = command bas_exe =<< getArgs
